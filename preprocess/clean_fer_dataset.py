bc05d88ebb4d07df0662e72eafb825e3a2b3da2f
NYY <liuyuan_tx@163.com>
2018-09-18 17:45:56 +0800

    clean dataset with azure emotion analyse API

--- preprocess/clean_fer_dataset.py
+++ preprocess/clean_fer_dataset.py
@@ -11,14 +11,10 @@ from PIL import Image
 import requests
 import shutil
 import time
+import json
 
 
 from dataset.load_fer2013_dataset import load_all_imagePath_label_list
-''''终结点: https://westcentralus.api.cognitive.microsoft.com/face/v1.0
-
-密钥 1: dc1104cfaa1d4f18859f5fbd6584c599
-
-密钥 2: 515a61302dc2408685b560b916d6b76e'''
 
 
 def save_data_as_images(csv_file, save_path, label_dict):
@@ -36,42 +32,7 @@ def save_data_as_images(csv_file, save_path, label_dict):
             im.save(image_name)
 
 
-def split_image_by_class(image_label_predict, image_path, clean_path):
-    subfloder = os.path.join(clean_path, image_label_predict)
-    if not os.path.exists(subfloder):
-        os.makedirs(subfloder)
-    new_iamge_name = os.path.basename(image_path)
-    new_image_save_file = os.path.join(subfloder, new_iamge_name)
-    shutil.copy(image_path, new_image_save_file)
-
-
-def clean_fer_image(image_path, clean_path):
-    subscription_key = '137de087e5004613b29d0c10419574c7'
-    emotion_recognition_url = "https://api.cognitive.azure.cn/face/v1.0/detect"
-    headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
-    params = {
-        'returnFaceId': 'true',
-        'returnFaceLandmarks': 'false',
-        'returnFaceAttributes': 'age,gender,smile,emotion'
-    }
-    data = open(image_path, 'rb')
-    img_url = {'url': image_path}
-    response = requests.post(emotion_recognition_url, params=params,
-                             headers=headers, data=data, json=img_url)
-    response.raise_for_status()
-    faces = response.json()
-    for face in faces:
-        fa = face['faceAttributes']
-        emotion = fa['emotion']
-        face_score = sorted(list(emotion.items()), key=lambda r: r[1], reverse=True)
-        image_label_predict = 'negative'
-        for key, value in face_score:
-            if value > 0.7:
-                image_label_predict = key
-        split_image_by_class(image_label_predict, image_path, clean_path)
-
-
-def add_blank_to_image(imagePath_list, label_list, save_dir):
+def padding_image(imagePath_list, label_list, save_dir):
     for i, imagePath in enumerate(imagePath_list):
         old_image = Image.open(imagePath)
         new_image = Image.new('L', (128, 128))
@@ -87,12 +48,77 @@ def add_blank_to_image(imagePath_list, label_list, save_dir):
     print('format %d images' % len(imagePath_list))
 
 
+def clean_fer_image(image_path, image_label, error_txt_file, norm_txt_file):
+    subscription_key = '137de087e5004613b29d0c10419574c7'
+    emotion_recognition_url = "https://api.cognitive.azure.cn/face/v1.0/detect"
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
+    params = {
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': 'age,gender,smile,emotion'
+    }
+    data = open(image_path, 'rb')
+    img_url = {'url': image_path}
+    response = requests.post(emotion_recognition_url, params=params,
+                             headers=headers, data=data, json=img_url)
+    save_string = ''
+    is_error = False
+    if response.status_code != requests.codes.ok:
+        error = response.json()
+        error = error['error']
+        error_code = error['code']
+        is_error = True
+        save_string = image_path + '\t' + image_label + '\t' + 'None' + '\t' + '0' + '\t' + error_code + '\n'
+        with open(error_txt_file, 'a') as fe:
+            fe.write(save_string)
+
+    else:
+        is_error = False
+        faces = response.json()
+        for face in faces:
+            fa = face['faceAttributes']
+            emotion = fa['emotion']
+            face_score = sorted(list(emotion.items()), key=lambda r: r[1], reverse=True)
+            image_label_predict, image_predict_score = face_score[0]
+            face_score_all = json.dumps(emotion)
+            save_string = image_path + '\t' + image_label + '\t' + image_label_predict + '\t' \
+                          + str(image_predict_score) + '\t' + face_score_all + '\t' + 'error_code' + '\n'
+            with open(norm_txt_file, 'a') as fn:
+                fn.write(save_string)
+
+    return save_string, is_error
+
+
+def save_return_info(info_list, save_txt_path):
+    info_file = open(save_txt_path, 'w')
+    for norm_info in info_list:
+        info_file.write(norm_info)
+    info_file.close()
+
+
+def creat_info_list(padidng_image_path_list, padding_label_list, error_txt_file, norm_txt_file):
+    error_str_list = []
+    norm_str_list = []
+    for i in range(34947, len(padding_label_list)):
+        save_string, is_error = clean_fer_image(padidng_image_path_list[i], padding_label_list[i], error_txt_file, norm_txt_file)
+        if is_error:
+            error_str_list.append(save_string)
+            print('error: %s' % padidng_image_path_list[i])
+
+        else:
+            norm_str_list.append(save_string)
+            print('normal: %s' % padidng_image_path_list[i])
+        time.sleep(5)
+    return error_str_list, norm_str_list
+
+
 if __name__ == '__main__':
     database_path = 'E:/liuyuan/DataCenter/DatasetFER2013/fer2013'
     csv_file = os.path.join(database_path, 'fer2013.csv')
     save_path = os.path.join(database_path, 'rebuild_images')
     addBlank_path = os.path.join(database_path, 'padding_images')
     clean_path = os.path.join(database_path, 'cleaned_images')
+
     label_dict = {0: 'anger', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'sad', 5: 'surprise', 6: 'neutral'}
     # save_data_as_images(csv_file, save_path, label_dict)
     # imagePath_list, label_list = load_all_imagePath_label_list(save_path)
@@ -101,9 +127,12 @@ if __name__ == '__main__':
 
     padidng_image_path_list, padding_label_list = load_all_imagePath_label_list(addBlank_path)
     # use microsoft api clean images
-    for i in range(len(padding_label_list)):
-        if i >= 4:
-            clean_fer_image(padding_label_list[i], clean_path)
-            time.sleep(15)
+    error_txt_file= os.path.join(clean_path, 'error_iamge.txt')
+    norm_txt_file = os.path.join(clean_path, 'norm_image.txt')
+    error_str_list, norm_str_list = creat_info_list(padidng_image_path_list, padding_label_list, error_txt_file, norm_txt_file)
+
+    # save_return_info(error_str_list, error_txt_file)
+    # save_return_info(norm_str_list, norm_txt_file)
+
 
 
